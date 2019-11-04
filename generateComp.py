bb7c3df31488bdc7e421c14d22abcd0b2c6995ad
ckuchman <kuch517@gmail.com>
2019-11-04 07:29:38 -0800

    Added horizon and started perspective

--- generateComp.py
+++ generateComp.py
@@ -9,6 +9,7 @@ MAX_HEIGHT = 1414
 
 random.seed()
 
+
 #----------Classes----------
 class Shape:
     def __init__(self, color):
@@ -38,11 +39,11 @@ class Triangle(Shape):
 
 
 #----------Functions----------
-def generateColor():
+def genColor():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
-def generateRandomCord(xMinRange, xMaxRange, yMinRange, yMaxRange):
+def genRandomCord(xMinRange, xMaxRange, yMinRange, yMaxRange):
     #Range is used to indicate where to constrain the shape with 1 being the full screen, numbers
     #above 1 are allowed to have shapes go off screen (so a 2 xMinRange would allow double the safe
     #to the left of the screen.
@@ -56,7 +57,7 @@ def generateRandomCord(xMinRange, xMaxRange, yMinRange, yMaxRange):
     return (xCord, yCord)
 
 
-def generateBackgroundPartial(draw):
+def genBackgroundPartial(draw):
     corners = [(0, 0), (MAX_WIDTH, 0), (0, MAX_HEIGHT), (MAX_WIDTH, MAX_HEIGHT)]
 
     random.shuffle(corners)
@@ -82,47 +83,86 @@ def generateBackgroundPartial(draw):
     cord1 = (xCord1, yCord1)
     cord2 = (xCord2, yCord2)
     
-    return draw.polygon([corner1, corner2, cord2, cord1], generateColor())
+    return draw.polygon([corner1, corner2, cord2, cord1], genColor())
 
 
-def generateRandomLine():
+def genRandomLine():
     minLineWidth = 20
     maxLineWidth = 80
-    cord1 = generateRandomCord(1.5, 1.5, 1.5, 1.5)
-    cord2 = generateRandomCord(1.5, 1.5, 1.5, 1.5)
+    cord1 = genRandomCord(1.5, 1.5, 1.5, 1.5)
+    cord2 = genRandomCord(1.5, 1.5, 1.5, 1.5)
 
-    return Line(generateColor(), random.randint(minLineWidth, maxLineWidth), cord1, cord2)
-
-
-def generateRandomTriangle():
-    cord1 = generateRandomCord(1.25, 1.25, 1.25, 1.25)
-    cord2 = generateRandomCord(1.25, 1.25, 1.25, 1.25)
-    cord3 = generateRandomCord(1.25, 1.25, 1.25, 1.25)
-
-    return Triangle(generateColor(), cord1, cord2, cord3)
+    return Line(genColor(), random.randint(minLineWidth, maxLineWidth), cord1, cord2)
 
 
-#----------Main Function----------
-#Generates the base image
-img = Image.new('RGB', (MAX_WIDTH, MAX_HEIGHT), color = generateColor())
-draw = ImageDraw.Draw(img)
+def genRandomTriangle():
+    cord1 = genRandomCord(1.25, 1.25, 1.25, 1.25)
+    cord2 = genRandomCord(1.25, 1.25, 1.25, 1.25)
+    cord3 = genRandomCord(1.25, 1.25, 1.25, 1.25)
 
-maxPartial = 2
-for i in range(random.randint(0, maxPartial)):
-    generateBackgroundPartial(draw)
-
-shapeList = []
-
-maxLineNum = 3
-for i in range(random.randint(0, maxLineNum)):
-    shapeList.append(generateRandomLine())
-
-maxTriangleNum = 3
-for i in range(random.randint(0, maxTriangleNum)):
-    shapeList.append(generateRandomTriangle())
+    return Triangle(genColor(), cord1, cord2, cord3)
 
 
-for shp in shapeList:
-    shp.draw(img)
+def genHorizon(draw, height): 
+    return draw.polygon([(0,0), (0,height), (MAX_WIDTH, height), (MAX_WIDTH, 0)], genColor())
 
-img.save('color.png')
+
+def genOnePointPerspective(draw, height):
+    xCord = random.randint(0, MAX_WIDTH)
+    
+    return (xCord, height)
+
+
+def genCubePerspective(draw, vanPnt, originPnt, width, height):
+    widthPnt = (originPnt[0] + width, originPnt[1])
+    heightPnt = (originPnt[0], originPnt[1] + height)
+    oppPnt = (originPnt[0] + width, originPnt[1] + height)
+
+    draw.polygon([originPnt, heightPnt, oppPnt, widthPnt], genColor())
+
+#----------Main Functions----------
+def randomImage():
+    #gens the base image
+    img = Image.new('RGB', (MAX_WIDTH, MAX_HEIGHT), color = genColor())
+    draw = ImageDraw.Draw(img)
+
+    maxPartial = 2
+    for i in range(random.randint(0, maxPartial)):
+        genBackgroundPartial(draw)
+
+    shapeList = []
+
+    maxLineNum = 3
+    for i in range(random.randint(0, maxLineNum)):
+        shapeList.append(genRandomLine())
+
+    maxTriangleNum = 3
+    for i in range(random.randint(0, maxTriangleNum)):
+        shapeList.append(genRandomTriangle())
+
+    for shp in shapeList:
+        shp.draw(img)
+
+    img.save('color.png')
+
+
+def perspectiveImage():
+    #gens the base image
+    img = Image.new('RGB', (MAX_WIDTH, MAX_HEIGHT), color = genColor())
+    draw = ImageDraw.Draw(img)
+
+    height = random.randint(0, MAX_HEIGHT)
+    genHorizon(draw, height)
+    vanPnt = genOnePointPerspective(draw, height)
+
+    genCubePerspective(draw, vanPnt, (100, 100), 100, 200)
+
+    shapeList = []
+
+    for shp in shapeList:
+        shp.draw(img)
+
+    img.save('horizon.png')
+
+#----------Main----------
+perspectiveImage()
